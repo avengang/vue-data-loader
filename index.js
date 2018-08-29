c82c32c8c2a2d882f5bab1ef1df63ba16eb7d148
avengang <544160071@qq.com>
2018-08-29 18:19:46 +0800

    init

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 module.exports = function(source) {
-	if(source.indexOf('@vue-data') !== -1) {
-		source = source.replace('@vue-data', ' new window.VueData(').replace('</script>', ')</script>')
-	}
+	source = source.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace('</script>', ')</script>')
   return source;
 };
