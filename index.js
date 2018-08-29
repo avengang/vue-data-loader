e5b2421aea3e37bc6fdee4f2626f4de995607b48
avengang <544160071@qq.com>
2018-08-29 18:26:17 +0800

    init

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = function(source) {
-	source = source.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace('</script>', ')</script>')
+	if(source.replace(/\s*/gm, '').indexOf('exportdefault') !== -1) {
+		source = source.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace('</script>', ')</script>')
+	} else {
+		source = source.replace('<script>', '<script>export default new window.VueData({').replace('</script>', '})</script>')
+	}
   return source;
 };
