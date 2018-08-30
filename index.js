798407e3d27642662ce83aceb4c5beef19973bc8
avengang <544160071@qq.com>
2018-08-30 10:45:12 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ module.exports = function(source) {
 	if(source.replace(/\s*/gm, '').indexOf('exportdefault') !== -1) {
 		source = source.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace('</script>', ')</script>')
 	} else {
-		source = source.replace('<script>', '<script>export default new window.VueData({').replace('</script>', '})</script>')
+		if(source.replace(/\s*/gm, '').indexOf('<script>') !== -1) {
+			source = source.replace('<script>', '<script>export default new window.VueData({').replace('</script>', '})</script>')
+		} else {
+			source = source + '<script>export default new window.VueData({})</script>'
+		}
 	}
   return source;
 };
