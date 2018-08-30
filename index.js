a34dab37878c3b28e87a2005175ca5394cd6ea44
avengang <544160071@qq.com>
2018-08-30 16:14:43 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 module.exports = function(source) {
-	if(source.replace(/\s*/gm, '').indexOf('exportdefault') !== -1) {
-		source = source.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace('</script>', ')</script>')
-	} else {
-		if(source.replace(/\s*/gm, '').indexOf('<script>') !== -1) {
-			source = source.replace('<script>', '<script>export default new window.VueData({').replace('</script>', '})</script>')
-		} else {
-			source = source + '<script>export default new window.VueData({})</script>'
+	var scriptIndex == source.lastIndexOf('<script>')
+	if(scriptIndex !== -1) { // 有<script>
+		var sourcepart1 = source.substring(0, scriptIndex)
+		var sourcepart2 = source.substr(scriptIndex)
+		
+		if(sourcepart2.replace(/\s*/gm, '').indexOf('exportdefault') !== -1) { // 有export default
+			sourcepart2 = sourcepart2.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace('</script>', ')</script>')
+		} else { // 没有export default
+			sourcepart2 = sourcepart2.replace('<script>', '<script>export default new window.VueData({').replace('</script>', '})</script>')
 		}
+		source = sourcepart1 + sourcepart2
+	} else { // 没有<script>
+		source = source + '<script>export default new window.VueData({})</script>'
 	}
   return source;
 };
