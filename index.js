859540f05b4fb95cd673a626de1eff3efc0d8bc1
avengang <544160071@qq.com>
2018-08-31 16:58:59 +0800

    修复文件脚本对象末尾有分号或逗号引起的bug

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ module.exports = function(source) {
 	if(scriptIndex !== -1) { // 有<script>
 		var sourcepart1 = source.substring(0, scriptIndex)
 		var sourcepart2 = source.substr(scriptIndex)
-		var tempIndex = sourcepart2.replace(/\s*/gm, '').lastIndexOf('};</script>') // 去掉分号
-		if(tempIndex !== -1) {
-			sourcepart2 = sourcepart2.substring(0, tempIndex) + '}</script>'
-		}
 		if(sourcepart2.replace(/\s*/gm, '').indexOf('exportdefault') !== -1) { // 有export default
-			sourcepart2 = sourcepart2.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace('</script>', ')</script>')
+			sourcepart2 = sourcepart2.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace(/}[\s]*[,|;]?[\s]*<\/script>/, '})</script>')
 		} else { // 没有export default
 			sourcepart2 = sourcepart2.replace('<script>', '<script>export default new window.VueData({').replace('</script>', '})</script>')
 		}
 		source = sourcepart1 + sourcepart2
 	} else { // 没有<script>
 		source = source + '<script>export default new window.VueData({})</script>'
+	}
+	var cpath = __dirname.replace("\\node_modules\\vue-storescanner-loader", "");
+	var componentPath = this.resourcePath.replace(cpath, "").replace(/\\/g, "_")
+		.replace(/\//g, "_").replace(".vue", "");
+		
+	if(componentPath.indexOf(":") != -1) {
+		componentPath = componentPath.split(":")[1];
 	}
   return source;
 };
