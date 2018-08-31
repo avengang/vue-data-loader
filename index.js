37d0064ac6ee1a32d772334396f82e163130264e
avengang <544160071@qq.com>
2018-08-31 16:18:40 +0800

    修复文件脚本对象末尾有分号的bug

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ module.exports = function(source) {
 	if(scriptIndex !== -1) { // 有<script>
 		var sourcepart1 = source.substring(0, scriptIndex)
 		var sourcepart2 = source.substr(scriptIndex)
-		
+		var tempIndex = sourcepart2.replace(/\s*/gm, '').lastIndexOf(';</script>') // 去掉分号
+		if(tempIndex !== -1) {
+			sourcepart2 = sourcepart2.substring(0, tempIndex) + '</script>'
+		}
 		if(sourcepart2.replace(/\s*/gm, '').indexOf('exportdefault') !== -1) { // 有export default
 			sourcepart2 = sourcepart2.replace(/export[\s]*default/gm, 'export default new window.VueData(').replace('</script>', ')</script>')
 		} else { // 没有export default
